a24b7d94d0d29840e58ee97203e546b9bf699148
joslinmicrosoft <54335291+joslinmicrosoft@users.noreply.github.com>
2020-03-25 17:18:30 -0700

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -24,9 +24,9 @@ function Home() {
         </a>
 		        <a 
           className="App-link"
-          href="/.auth/login/facebook"
+          href="/.auth/login/google"
         >
-        Log in with Facebook
+        Log in with Google
         </a>
 
       </header>
